5d960522a7f15647d4aab37243c032fda7fcef0a
Karla Hugo <karlabrandon817@gmail.com>
2018-01-23 16:28:27 +0000

    user login and logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,24 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  #remembers user in persistent session
+  def remember(user)
+    user.remember_token
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   #returns current logged in user (if any)
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
   
   #returns true if the user is logged in
@@ -13,7 +28,15 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  #forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
